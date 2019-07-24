6184f27b397b1a032e804b195ee17340917498ec
SelloClinton <680492@students.wits.ac.za>
2019-07-24 16:35:39 +0200

    added mosfets on motor terminals, speed can be regulated

--- HBRIDGE/HBRIDGE.ino
+++ HBRIDGE/HBRIDGE.ino
@@ -1,26 +1,24 @@
 #define pwmPin3 3
-#define pwmPin5 5
+#define pwmPin11 11
 #define pwmPin6 6
 #define pwmPin9 9
 void setup() {
  pinMode(pwmPin3,OUTPUT);
- pinMode(pwmPin5,OUTPUT);
+ pinMode(pwmPin11,OUTPUT);
  pinMode(pwmPin6,OUTPUT);
  pinMode(pwmPin9,OUTPUT);
+ TCCR2B = TCCR2B & B11111000 | B00000001;
 }
 
 void loop() {
 
-        analogWrite(pwmPin3,0);
-        analogWrite(pwmPin5,255);
-        delay(3000);
-        analogWrite(pwmPin5,200);
-        delay(3000);
-        analogWrite(pwmPin5,150);
-        delay(3000);
-        analogWrite(pwmPin5,135);
-        delay(3000);
-        analogWrite(pwmPin5,0);
-        delay(3000);
-        
+      analogWrite(pwmPin6,255);
+      analogWrite(pwmPin9,0);
+      digitalWrite(pwmPin11,HIGH);
+      analogWrite(pwmPin3,255);
+      delay(2000);
+      analogWrite(pwmPin3,235);
+      delay(5000);
+
+      
 }
