1cd1ec7527ca729ebf5515c42d84a23da2f9a368
SelloClinton <680492@students.wits.ac.za>
2019-08-14 20:40:41 +0200

    gap sensor encoding code revisited

--- encoding/encoding.ino
+++ encoding/encoding.ino
@@ -1,36 +1,32 @@
-#define pwmPin3 3
-#define pwmPin5 5
-int sensor 3
-volatile int counter = 0;
+#define s1 3
 volatile int revolution = 0;
-float timer = 0.0;
-
-
+volatile int counter = 0;
+volatile int timing = 0;
 void setup(){
-  pinMode(pwmPin3,OUTPUT);
-  pinMode(pwmPin5,OUTPUT);
-  pinMode(s1,INPUT);
-  attachInterrupt(digitalPinToInterrupt(sensor),gap,CHANGE);
-  Serial.begin(9600);
-}
-
+    pinMode(s1,INPUT);
+    pinMode(10,OUTPUT);
+    pinMode(A0,INPUT);
+    pinMode(A1,INPUT);
+    Serial.begin(9600); 
+    attachInterrupt(digitalPinToInterrupt(s1), printS, CHANGE);
+ }
+//
 void loop(){
-//  analogWrite(pwmPin3,0);
-//  analogWrite(pwmPin5,255);
-    timer = milis();
-  
-  
+    millis();
 }
-
-void gap(){
-    if(digitalRead(sensor) == 0){
-        counter++;
-      }
-    if(counter % 80 == 0){
-        revolution++
+//
+void printS(){
+  if(digitalRead(s1) == 0){
+      counter++; 
+   }
+    revolution = counter / 80;
+    
+   if(counter == 80){
+        revolution++;
+        counter = 0;
         Serial.print(revolution);
-        Serial.println("REVOLUTIONS");
-      
-      }
-  
+        Serial.println(" REVOLUTIONS");
+    }
+
+   
 }
