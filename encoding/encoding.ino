786f953b18bf0b4b8958b687a701ddd5e7f46bc1
SelloClinton <680492@students.wits.ac.za>
2019-08-19 20:02:59 +0200

    encoder-shaft rpm

--- encoding/encoding.ino
+++ encoding/encoding.ino
@@ -1,39 +1,111 @@
-#define s1 3
- volatile byte half_revolutions;
- unsigned int rpm;
- unsigned long timeold;
- unsigned int previous;
- unsigned int count = 0;
- void setup()
- {
-   Serial.begin(9600);
-   attachInterrupt(digitalPinToInterrupt(s1), rpm_fun, RISING);
-   half_revolutions = 0;
-   rpm = 0;
-   timeold = 0;
-   //Serial.println(rpm,DEC)
- }
- void loop()
- {  
-  if(half_revolutions >= 80) { 
-     //Update RPM every 20 counts, increase this for better RPM resolution,
-     //decrease for faster update
-      rpm = (30*1000/(millis() - timeold)*half_revolutions)/(40);
-      timeold = millis();
-      Serial.println(rpm,DEC);
-      count = half_revolutions; 
-      half_revolutions = 0;     
-  }
-  
- // Serial.println(count,DEC);
- 
-}
+ /*
+  Gearmotor Rotary Encoder Test
+  motor-encoder-rpm.ino
+  Read pulses from motor encoder to calculate speed
+  Control speed with potentiometer
+  Displays results on Serial Monitor
+  Use Cytron MD10C PWM motor controller
+  DroneBot Workshop 2019
+  https://dronebotworkshop.com
+*/
 
-void rpm_fun()
+// Motor encoder output pulse per rotation (change as required)
+#define ENC_COUNT_REV 400
+
+// Encoder output to Arduino Interrupt pin
+#define ENC_IN 2 
+
+// MD10C PWM connected to pin 10
+#define PWM 10 
+// MD10C DIR connected to pin 12
+#define DIR 12 
+
+// Analog pin for potentiometer
+int speedcontrol = 0;
+
+// Pulse count from encoder
+volatile long encoderValue = 0;
+
+// One-second interval for measurements
+int interval = 100;
+
+// Counters for milliseconds during interval
+long previousMillis = 0;
+long currentMillis = 0;
+
+// Variable for RPM measuerment
+int rpm = 0;
+
+// Variable for PWM motor speed output
+int motorPwm = 0;
+float rpm_array[10];
+
+void setup()
 {
-   half_revolutions++;
-   //Each rotation, this interrupt function is run twice
-   
+  // Setup Serial Monitor
+  Serial.begin(9600); 
+  
+  // Set encoder as input with internal pullup  
+  pinMode(ENC_IN, INPUT_PULLUP); 
+
+  // Set PWM and DIR connections as outputs
+  pinMode(PWM, OUTPUT);
+  pinMode(DIR, OUTPUT);
+  
+  // Attach interrupt 
+  attachInterrupt(digitalPinToInterrupt(ENC_IN), updateEncoder, RISING);
+  
+  // Setup initial values for timer
+  previousMillis = millis();
 }
 
-//optimal control by bell upto ch6
+void loop()
+{
+  
+    // Control motor with potentiometer
+    motorPwm = map(analogRead(speedcontrol), 0, 1023, 0, 255);
+    
+    // Write PWM to controller
+    analogWrite(PWM, motorPwm);
+  
+  // Update RPM value every second
+  currentMillis = millis();
+    float ave = 0.0;
+  float sum = 0.0;
+ // for(int i = 0; i != 10; i++){
+  if (currentMillis - previousMillis > interval) {
+    previousMillis = currentMillis;
+
+
+    // Calculate RPM
+    rpm = (float)(encoderValue * 60 / ENC_COUNT_REV);
+ //   rpm_array[i] = rpm;
+//    sum+=rpm_array[j];
+    // Only update display when there is a reading
+    if (motorPwm > 0 || rpm > 0) {
+      Serial.print("PWM VALUE: ");
+      Serial.print(motorPwm);
+      Serial.print('\t');
+      Serial.print(" PULSES: ");
+      Serial.print(encoderValue);
+      Serial.print('\t');
+      Serial.print(" SPEED: ");
+      Serial.print(rpm);
+      Serial.println(" RPM");
+    }
+    
+    encoderValue = 0;
+  }
+//  }
+
+//    ave = sum/10.0;
+//    Serial.print("ave_RPM : ");
+//    Serial.println(ave);
+
+}
+
+void updateEncoder()
+{
+  // Increment value for each pulse from encoder
+  encoderValue++;
+}
