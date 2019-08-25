ce6bb2ff334bbdb4acfe5f8689a8d1748d208a2e
SelloClinton <680492@students.wits.ac.za>
2019-08-25 22:07:09 +0200

    ...

---
+++ dc_motor_simulink_modelling.m
@@ -0,0 +1,26 @@
+J = 0.010;
+b = 0.568;
+K = 1;
+R = 1;
+L = 1;
+counter = int32(0);
+voltage = int32(0);
+revolution = int32(0);
+rev_distance = int32(5);
+rd = (pi/30);
+
+%obtained
+K_e = 0.24+0.0080;
+K_t = 0.36;
+R_a = 3.5017;
+L_a = 0.00010;
+
+K_g = 1.67; 
+pinion_radius = 0.175;
+mass = 2.50;
+g = 9.89;
+fr_coeff = 0.8;
+F_fr = fr_coeff*(mass*g);
+
+
+
