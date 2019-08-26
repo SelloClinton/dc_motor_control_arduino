2d2e4b4af83aea7dfad421207a93152f4449a3c7
SelloClinton <680492@students.wits.ac.za>
2019-08-26 10:52:17 +0200

    logic v1.0

--- PID/PID.ino
+++ PID/PID.ino
@@ -2,8 +2,10 @@
 #define ENC_IN 2
 #define PID_OUT 6
 #define ENC_COUNT_REV 400
+#define BUTTON 7
 
 volatile long encoderValue = 0;
+volatile long counter = 0;
 unsigned long currentTime, previousTime;
 double elapsedTime;
 double error;
@@ -28,25 +30,46 @@ double rpm_array[] = {124.0,150.0,154.0,166.0,168.0,188.0,190.0,200.0};
 
 //P gain
 double Kp = 0.0708;
-double Ki = 1;
-double Kd = 1;
+double Ki = 0;
+double Kd = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(BUTTON,INPUT_PULLUP);
   pinMode(ENC_IN,INPUT_PULLUP);
   pinMode(PID_OUT,OUTPUT);
-  setPoint = 3600;
+  setPoint = 23532; // 1.59 pinion_revs
   previousMillis = millis();
   attachInterrupt(digitalPinToInterrupt(ENC_IN), updateEncoder, RISING);  
+ // digitalWrite(7,0);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-//  pickController();
-//  volt_command();  
-    computeRPM();
+ //    computeRPM();
+
+    
+ //   pickController();
+//    Serial.print("counter: ");
+//    Serial.println(counter);
+ //   volt_command();  
+    while(digitalRead(BUTTON) == HIGH){
+        Serial.println("WAITING FOR BUTTON PRESS...");
+      }
+      Serial.println("BUTTON PRESSED...");
+      counter = 1;
+      while(setPoint - counter > 0){
+          computePID(counter);
+          volt_command();
+          Serial.print("counter: ");
+          Serial.print(counter);
+          Serial.print(" --- PWM_OUT: ");
+          Serial.println(output);
+        }
+//Serial.println()
+    
 }
 
 double computePID(double inp){
@@ -55,7 +78,7 @@ double computePID(double inp){
     elapsedTime = (double)(currentTime - previousTime);
     
 //
-    error = setPoint - encoderValue;
+    error = setPoint - counter;
     cum_error += error * elapsedTime;
     rateError = (error - lastError)/elapsedTime; 
     double out = Kp*error; + Ki*cum_error + Kd*rateError;
@@ -66,32 +89,27 @@ double computePID(double inp){
  }
 void volt_command(){
   
-//   updateEncoder();
-  output = computePID(encoderValue);
+
+  output = computePID(counter);
 
   delay(100);
-  if((encoderValue - eps < 0) || (setPoint  - encoderValue < eps )){
+  if((counter - eps < 0) || (setPoint  - counter < eps )){
     output = 0;
- //     analogWrite(PID_OUT,output);
-      
     }
    else if(output > 255.0){
-   //   lastOut = 255.0;
       output = max;
-//      analogWrite(PID_OUT,output);
-       
    }
    else if (output < 160.0){
         output = min;
     }
   analogWrite(PID_OUT,output); 
-  Serial.print("encoder value : ");
-  Serial.print(encoderValue);
+//  Serial.print("counter value : ");
+//  Serial.print(counter);
   Serial.print(" --- PID_OUTPUT : ");
   Serial.println(output);
  
   
-  delay(1000);
+//  delay(1000);
  
 }
 
@@ -142,13 +160,14 @@ void computeRPM(){
 //     Calculate RPM
         rpm = (float)(encoderValue * 60 / ENC_COUNT_REV);
         encoderValue = 0;
-        Serial.print("RPM: ");
-        Serial.println(rpm);
+//        Serial.print("RPM: ");
+//        Serial.println(rpm);
       }
 }
 
 void updateEncoder(){
 
   encoderValue++;
+  counter++;
 
 }
